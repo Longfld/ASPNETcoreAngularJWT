54941cb27527ad13dacb964d303363c8d7be39dc
Longfld <Longfld@users.noreply.github.com>
2017-04-06 11:18:42 +1000

    Update app.routing.ts

--- wwwroot/app/app.routing.ts
+++ wwwroot/app/app.routing.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './Login/login.component';
 import { SecretComponent } from './secret/secret.component';
-import { AuthService} from './shared/auth.service'
+import { AuthService} from './shared/auth.service';
 
 const appRoutes: Routes = [
     { path: "", redirectTo: "login", pathMatch: "full" },
