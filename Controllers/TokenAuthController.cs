3eaab799e129ea3336436bc8212c6935afc8176d
Longfld <Longfld@users.noreply.github.com>
2017-04-06 10:57:53 +1000

    init.

---
+++ Controllers/TokenAuthController.cs
@@ -0,0 +1,89 @@
+using System;
+//using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace ASPNETCoreAngularJWT
+{
+
+    [Route("api/[controller]")]
+    public class TokenAuthController : Controller
+    {
+
+       
+        [HttpPut("Login")]
+        public IActionResult Login([FromBody]User user)
+        {
+            User existUser = UserStorage.Users.FirstOrDefault(u => u.Username == user.Username && u.Password == user.Password);
+
+            if (existUser != null)
+            {
+
+                var requestAt = DateTime.Now;
+                var expiresIn = requestAt + TokenAuthOption.ExpiresSpan;
+                var token = GenerateToken(existUser, expiresIn);
+
+                return Json(new RequestResult
+                {
+                    State = RequestState.Success,
+                    Data = new
+                    {
+                        requertAt = requestAt,
+                        expiresIn = TokenAuthOption.ExpiresSpan.TotalSeconds,
+                        tokeyType = TokenAuthOption.TokenType,
+                        accessToken = token
+                    }
+                });
+            }
+            else
+            {
+                return Json(new RequestResult
+                {
+                    State = RequestState.Failed,
+                    Msg = "Username or password is invalid"
+                });
+            }
+        }
+
+        private string GenerateToken(User user, DateTime expires)
+        {
+            var handler = new JwtSecurityTokenHandler();
+
+            ClaimsIdentity identity = new ClaimsIdentity(
+                new GenericIdentity(user.Username, "TokenAuth"),
+                new[] {
+                    new Claim("ID", user.ID.ToString())
+                }
+            );
+
+            var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = TokenAuthOption.Issuer,
+                Audience = TokenAuthOption.Audience,
+                SigningCredentials = TokenAuthOption.SigningCredentials,
+                Subject = identity,
+                Expires = expires
+            });
+            return handler.WriteToken(securityToken);
+        }
+
+        [HttpGet]
+        [Authorize("Bearer")]
+        public IActionResult GetUserInfo()
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            return Json(new RequestResult
+            {
+                State = RequestState.Success,
+                Data = new { UserName = claimsIdentity.Name }
+            });
+        }
+    }
+}
+
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreAngularJWT
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [AllowAnonymous]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        [HttpGet("GetStaff")]
+        [Authorize("Bearer")]
+        public IActionResult GetStaff()
+        {
+            List<string> model = new List<string>();
+            foreach (User user in UserStorage.Users ){
+               model.Add(user.Username);
+            }
+            return Json(model);
+        }
+    }
+}
