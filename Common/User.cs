5fc6c85abbf4754935e7d54ffbbb2ecb04b27bf2
Longfld <Longfld@users.noreply.github.com>
2017-04-06 10:51:56 +1000

    Create User.cs

---
+++ Common/User.cs
@@ -0,0 +1,19 @@
+ using System;
+
+
+ namespace ASPNETCoreAngularJWT
+{
+  public class User
+    {
+        public Guid ID { get; set; }
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Fname {get;set;}
+        public string Lname {get;set;}
+    
+    }
+   
+   
+}
