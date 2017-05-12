bf384f4b85b7b566919ec0221a0a11b39998f6f6
Longfld <Longfld@users.noreply.github.com>
2017-05-12 15:03:26 +1000

    For linux

--- wwwroot/app/app.module.ts
+++ wwwroot/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent }  from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { routing } from './app.routing';
-import { LoginComponent } from './Login/login.component';
+import { LoginComponent } from './login/login.component';
 import { SecretComponent } from './secret/secret.component';
 import { AuthService } from "./shared/auth.service";
 
