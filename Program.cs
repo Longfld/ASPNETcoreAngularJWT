e6e7c6a56d93dedf2d8d122f560ea5ee63e45e27
Longfld <Longfld@users.noreply.github.com>
2017-08-22 09:28:26 +1000

    asp.net core 2 upgrade

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace ASPNETCoreAngularJWT
 {
@@ -12,14 +14,29 @@ namespace ASPNETCoreAngularJWT
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
+            public static void Main(string[] args)
+        {
+            var webHost = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                })
                 .UseStartup<Startup>()
                 .Build();
 
-            host.Run();
+            webHost.Run();
+        }
         }
     }
 }
