128ef8c08e6f06d86a93a4b2164d8a2dc9ff6709
Longfld <Longfld@users.noreply.github.com>
2017-08-22 12:03:33 +1000

    AddSerilog for asp.net core 2

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace ASPNETCoreAngularJWT
 {
@@ -23,10 +24,13 @@ namespace ASPNETCoreAngularJWT
                     config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                           .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
                     config.AddEnvironmentVariables();
+                    
+                    Log.Logger = new LoggerConfiguration().MinimumLevel.Error().WriteTo.RollingFile(Path.Combine(env.ContentRootPath,"logs/{Date}.txt")).CreateLogger();
                 })
                 .ConfigureLogging((hostingContext, logging) =>
                 {
                     logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddSerilog();
                     logging.AddConsole();
                     logging.AddDebug();
                 })
