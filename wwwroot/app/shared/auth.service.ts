cb89f69f8e6598793f210d79be524b4a8266458f
Longfld <Longfld@users.noreply.github.com>
2017-10-12 14:23:36 +1100

    HttpClient

--- wwwroot/app/shared/auth.service.ts
+++ wwwroot/app/shared/auth.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from "@angular/core";
-import { Headers, Http, Response, RequestOptions } from "@angular/http";
-
+import { HttpClient,HttpHeaders} from "@angular/common/http";
 import { Router, CanActivate } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
-//import { RequestResult } from "./RequestResult";
+
+import {AuthBearer} from './AuthBearerInterface';
 
 @Injectable()
 export class AuthService implements CanActivate {
     private tokeyKey = "token";
 
-    constructor(private http: Http, private router: Router) { }
+    constructor(private http: HttpClient, private router: Router) { }
 
     public canActivate() {
         if (this.checkLogin()) {
@@ -22,30 +22,26 @@ export class AuthService implements CanActivate {
     }
 
     public login$(userName: string, password: string) {
-        let header = new Headers({ 'Content-Type': 'application/json' });
+        let header = new HttpHeaders().set('Content-Type', 'application/json');
         let body = JSON.stringify({ "Username": userName, "Password": password });
-        let options = new RequestOptions({ headers: header });
+        let options = {headers:header};
 
-        return this.http.put("/api/TokenAuth/Login", body, options).map(
+        return this.http.put<AuthBearer>("/api/TokenAuth/Login", body, options).map(
             res => {
-                let result = res.json();
-                if (result.state == 1 && result.data && result.data.accessToken) {
+                let result = res;
+                if (result.state && result.state == 1 && result.data && result.data.accessToken) {
                     sessionStorage.setItem(this.tokeyKey, result.data.accessToken);
                 }
                 return result;
             }
-        ).catch(this.handleError);
+        ).shareReplay().catch(this.handleError);
     }
 
     public authGet$(url) {
-        let headers = this.initAuthHeaders();
-        let options = new RequestOptions({ headers: headers });
-        return this.http.get(url, options).map(
-            response => response.json()
-        ).catch(this.handleError);
+        let header = this.initAuthHeaders();
+        let options = {headers:header};
+        return this.http.get(url, options).shareReplay().catch(this.handleError);
     }
-
-   
 
     public checkLogin(): boolean {
         let token = sessionStorage.getItem(this.tokeyKey);
@@ -53,31 +49,32 @@ export class AuthService implements CanActivate {
     }
 
     public getUserInfo$() {
-        return this.authGet$("/api/TokenAuth");
+        return this.authGet$("/api/TokenAuth").shareReplay();
     }
 
     public authPost$(url: string, body: any) {
         let headers = this.initAuthHeaders();
-        return this.http.post(url, body, { headers: headers }).map(response => response.json()).catch(this.handleError);
+        return this.http.post(url, body, { headers: headers }).shareReplay().catch(this.handleError);
     }
 
     private getLocalToken(): string {
            return sessionStorage.getItem(this.tokeyKey);
     }
 
-    private initAuthHeaders(): Headers {
+    private initAuthHeaders(): HttpHeaders {
         let token = this.getLocalToken();
         if (token == null) throw "No token";
 
-        let headers = new Headers({ 'Content-Type': 'application/json' });
-        headers.append("Authorization", "Bearer " + token);
+        let headers = new HttpHeaders()
+                        .set('Content-Type','application/json')
+                        .set("Authorization", "Bearer " + token);
         return headers;
     }
 
     private handleError(error: any) {
-        let errMsg = (error.message) ? error.message :
-            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+        let errMsg = (error.message) ? error.message : "Server error";
         console.error(errMsg);
         return Observable.throw(errMsg);
     }
 }
+
