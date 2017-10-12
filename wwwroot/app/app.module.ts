31d744fd2007a2cc63e0dcc2c6ea1b1d2ab5777a
Longfld <Longfld@users.noreply.github.com>
2017-10-12 14:25:05 +1100

    HttpClient

--- wwwroot/app/app.module.ts
+++ wwwroot/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent }  from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -17,7 +17,7 @@ import 'hammerjs';
     imports: [
         BrowserModule,
         FormsModule,
-        HttpModule,
+        HttpClientModule,
         routing
     ],
     providers: [AuthService],
