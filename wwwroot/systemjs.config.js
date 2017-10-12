2d228d16e24bcfcf16bd9b2057ed08bdf72a2cc6
Longfld <Longfld@users.noreply.github.com>
2017-10-12 14:26:09 +1100

    HttpClient

--- wwwroot/systemjs.config.js
+++ wwwroot/systemjs.config.js
@@ -14,7 +14,7 @@
             '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
             '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
             '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-            '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+            '@angular/common/http': 'npm:@angular/common/bundles/common-http.umd.js',
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
             
@@ -22,7 +22,8 @@
              '@angular/cdk': 'npm:@angular/cdk/bundles/cdk.umd.js',
             // other libraries
             'rxjs': 'npm:rxjs',
-            'hammerjs': 'npm:hammerjs'
+            'hammerjs': 'npm:hammerjs',
+            'tslib': 'npm:tslib/tslib.js'
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
