28364a26f882ce5fe762252d6a42af66fb19aaec
Longfld <Longfld@users.noreply.github.com>
2017-10-25 12:00:48 +1100

    material beta 12

--- wwwroot/app/app.module.ts
+++ wwwroot/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
