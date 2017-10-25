88e77c338a805d0eb7b820b894905d705c2a1204
Longfld <Longfld@users.noreply.github.com>
2017-10-25 11:46:15 +1100

    material beta 12

--- wwwroot/app/app.module.ts
+++ wwwroot/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MyMaterialModule} from './my.material.moudule'
 import { AppComponent }  from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
@@ -11,13 +12,15 @@ import { LoginComponent } from './login/login.component';
 import { SecretComponent } from './secret/secret.component';
 import { AuthService } from "./shared/auth.service";
 
-import 'hammerjs';
+import 'hammerjs/hammer';
 
 @NgModule({
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         FormsModule,
         HttpClientModule,
+        MyMaterialModule,
         routing
     ],
     providers: [AuthService],
