418d9f337755c771040e9bc941479cb04e795e3e
Longfld <Longfld@users.noreply.github.com>
2018-06-29 10:49:42 +1000

    angular 6

--- wwwroot/app/main.ts
+++ wwwroot/app/main.ts
@@ -1,7 +1,6 @@
 //import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
-import 'rxjs/Rx';
 import 'hammerjs/hammer';
 
 
